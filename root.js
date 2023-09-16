bc9080bc78780973bbe428963281b761086dd41b
asdf1242 <12887-Twentytwo@users.noreply.gitgud.io>
2023-09-15 23:27:02 -0700

    add base website files

---
+++ root.js
@@ -0,0 +1,6 @@
+setInterval(randomBackground, 1000);
+
+function randomBackground() {
+	const randomColor = Math.floor(Math.random()*16777215).toString(16);
+	document.body.style.backgroundColor = "#" + randomColor;
+}
