2212fb59e278f51c9e7a1754e43280d7fb4004e6
asdf1242 <12887-Twentytwo@users.noreply.gitgud.io>
2023-09-21 23:54:47 -0700

    HTML interface is now injected via JavaScript. Buttons now bring users to a different screen

--- root.js
+++ root.js
@@ -1,4 +1,6 @@
 // Set up connection to Websocket + send/receive behaviors
+window.onload = Init
+
 window.addEventListener("DOMContentLoaded", () => {
 	const websocket = new WebSocket("ws://localhost:8001/");
 	receiveTable(websocket);
@@ -10,19 +12,29 @@ function receiveTable(websocket) {
 	})
 }
 
-// Change screen
-function Start() {
-
+//Called when need to go to main menu. 
+function Init() {
+	//Might take up more memory by not calling Home() but on the other hand will increase speed.
+	document.getElementById("Interface").innerHTML = "<div class = \"centered\"><div id = \"menu\"><h1> Virus Simulator </h1><button class=\"button\" onclick=\"StartGame()\">Start Game</button><br><button class=\"button\" onclick=\"Set()\">Settings</button></div></div>"
 }
 
+// Display Setting interface
 function Set() {
-
+	document.getElementById("menu").innerHTML = "<h1> Settings </h1><br><br><p style=\"color:white\">Nothing to see here for now</p><button class=\"button\" onclick=\"Home()\">Back</button>"
 }
 
 function Home() {
-
+	//Flex attempts to split Centered container into (number of elements) areas horizontally. div below "centered" prevents horizontal distribution/splitting
+	document.getElementById("menu").innerHTML = "<h1> Virus Simulator </h1><button class=\"button\" onclick=\"StartGame()\">Start Game</button><br><button class=\"button\" onclick=\"Set()\">Settings</button>"
 }
 
-function Exit() {
+// Display game interface
+function StartGame() {
+	//document.getElementById("Interface").innerHTML = 
+	End()
+}
 
+// Interface when game ends
+function End() {
+	document.getElementById("Interface").innerHTML = "<button class=\"button\" onclick=\"Init()\">Return to Main Menu</button>"
 }
